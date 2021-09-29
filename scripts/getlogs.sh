3b516ecfd5b1d83b6d3e4153557a55e048876273
Dominic Rudigier <dru@pro.nohost.me>
2021-09-29 12:42:27 +0200

    CLEANUP: removed leftovers

--- scripts/getlogs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# /home/cowrie/cowrie/var/log/cowrie
-# honeypot a
-scp -P 2112 -r root@104.248.253.81:/home/cowrie/cowrie/var/log/cowrie/*.json.* /Users/dominicrudigier/Documents/university/01\ UNI/06\ Semester/Bachelorarbeit/scripts/project/logs
-scp -P 2112 -r root@104.248.253.142:/home/cowrie/cowrie/var/log/cowrie/*.json.* /Users/dominicrudigier/Documents/university/01\ UNI/06\ Semester/Bachelorarbeit/scripts/project/logs
-scp -P 2112 -r root@104.248.245.133:/home/cowrie/cowrie/var/log/cowrie/*.json.* /Users/dominicrudigier/Documents/university/01\ UNI/06\ Semester/Bachelorarbeit/scripts/project/logs
-
-
